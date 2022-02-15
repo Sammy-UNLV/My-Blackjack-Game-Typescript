2f8adeed598640a04a59b8e5b040ef25c6ffd649
Samuel FIsher <sammykiy@gmail.com>
2022-02-14 23:58:02 -0800

    Added some formatting

--- assets/java.js
+++ assets/java.js
@@ -1,1 +1,9 @@
+// Prompt User to Enter thier name
 
+//Update Name
+
+//Click button to start asgame will load without cards
+
+//Player will start with $1000
+
+// Each Bet is $20. Beetting up will increase by $20
